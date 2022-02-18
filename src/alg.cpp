b29b69106908ff2ea44ebbec3fe438378b0f5a3c
EgorShanin <94217235+EgorShanin@users.noreply.github.com>
2022-02-19 01:02:26 +0300

    EgorShanin

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,15 +5,14 @@
 
 bool checkPrime(uint64_t value) {
     uint64_t count = 0;
-    for(uint64_t i = 1; i < value; i++) {
-        if(value % i == 0) {
+    for (uint64_t i = 1; i < value; i++) {
+        if (value % i == 0) {
             count++;
         }
     }
-    if(count == 1) {
+    if (count == 1) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
@@ -22,25 +21,24 @@ uint64_t nPrime(uint64_t n) {
     uint64_t count = 0;
     uint64_t countPrimes = 0;
     uint64_t num = 0;
-    for(uint64_t i = 1; ; i++) {
-        for(uint64_t j = 1; j < i ; j++) {
-            if(i % j == 0) {
+    for (uint64_t i = 1; ; i++) {
+        for (uint64_t j = 1; j < i ; j++) {
+            if (i % j == 0) {
                 count++;
             }
         }
-        if(count == 1 ) {
+        if (count == 1) {
             countPrimes++;
         }
         count = 0;
-        if(countPrimes == n) {
+        if (countPrimes == n) {
             num = i;
             break;
         }
     }
-    if(num != 1) {
+    if (num != 1) {
         return num;
-    }
-    else{
+    } else {
         return 0;
     }
 }
@@ -50,41 +48,39 @@ uint64_t nextPrime(uint64_t value) {
     uint64_t countHigh = 0;
     uint64_t numLow = 0;
     uint64_t numHigh = 0;
-    if(value == 2) {
+    if (value == 2) {
         return 3;
     }
-    if(value < 2) {
+    if (value < 2) {
         return 2;
-    }
-    else{
-        for(uint64_t i = (value - 1); ; i--) {
-            for(uint64_t j = 1; j < i; j++) {
-                if(i % j == 0) {
+    } else {
+        for (uint64_t i = (value - 1); ; i--) {
+            for (uint64_t j = 1; j < i; j++) {
+                if (i % j == 0) {
                     countLow++;
                 }
             }
-            if(countLow == 1) {
+            if (countLow == 1) {
                 numLow = i;
                 break;
             }
             countLow = 0;
         }
-        for(uint64_t i = (value + 1); ; i++) {
-            for(uint64_t j = 1; j < i; j++) {
-                if(i % j == 0) {
+        for (uint64_t i = (value + 1); ; i++) {
+            for (uint64_t j = 1; j < i; j++) {
+                if (i % j == 0) {
                     countHigh++;
                 }
             }
-            if(countHigh == 1) {
+            if (countHigh == 1) {
                 numHigh = i;
                 break;
             }
             countHigh = 0;
         }
-        if((value - numLow) < (numHigh - value)) {
+        if ((value - numLow) < (numHigh - value)) {
             return numLow;
-        }
-        else {
+        } else {
             return numHigh;
         }
     }
@@ -93,13 +89,13 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t count = 0;
     uint64_t sum = 0;
-    for(uint64_t i = 0; i < hbound; i++) {
-        for(uint64_t j = 1; j < i; j++) {
-            if(i % j == 0) {
+    for (uint64_t i = 0; i < hbound; i++) {
+        for (uint64_t j = 1; j < i; j++) {
+            if (i % j == 0) {
                 count++;
             }
         }
-        if(count == 1) {
+        if (count == 1) {
             sum += i;
         }
         count = 0;
